a6b04810e8ddddabdbff6b45983d9d558648e40a
AnahiHerrera <151777158+AnahiHerrera@users.noreply.github.com>
2023-12-01 00:48:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+let intentos = 6;
+let palabra = "APPLE"
+
+window.addEventListener('load', init);
+
+function init() {
+    console.log('Esto se ejecuta solo cuando se carga la página web');
+}
+
+const button = document.getElementById("guess-button");
+button.addEventListener("click", intentar);
+
+function intentar() {
+    const INTENTO = leerIntento();
+
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
+
+    for (let i in palabra) {
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+
+        if (INTENTO[i] === palabra[i]) {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'green';
+        } else if (palabra.includes(INTENTO[i])) {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'yellow';
+        } else {
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'grey';
+        }
+        ROW.appendChild(SPAN);
+    }
+    GRID.appendChild(ROW);
+
+    if (INTENTO === palabra) {
+        terminar("<h1>¡GANASTE! 😄</h1>");
+        return;
+    }
+
+    for (let i in palabra) {
+        if (INTENTO[i] === palabra[i]) {
+            console.log(INTENTO[i], "VERDE");
+        } else if (palabra.includes(INTENTO[i])) {
+            console.log(INTENTO[i], "AMARILLO");
+        } else {
+            console.log(INTENTO[i], "GRIS");
+        }
+    }
+
+    intentos--;
+
+    if (intentos === 0) {
+        terminar("<h1>¡PERDISTE! 😔</h1>");
+    }
+}
+
+function terminar(mensaje) {
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    button.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+}
+
+function leerIntento() {
+    let intento = document.getElementById("guess-input");
+    intento = intento.value;
+    intento = intento.toUpperCase();
+    return intento;
+}
